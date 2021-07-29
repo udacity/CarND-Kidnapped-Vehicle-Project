52b257d75160e39576b88ca0d0eceddf2a294dc6
Peravene Raveendran <peravene@yahoo.de>
2021-07-29 22:13:45 +0200

    Fix warning in main

--- src/main.cpp
+++ src/main.cpp
@@ -100,7 +100,7 @@ int main() {
           std::istream_iterator<float>(),
           std::back_inserter(y_sense));
 
-          for (int i = 0; i < x_sense.size(); ++i) {
+          for (long unsigned int i = 0; i < x_sense.size(); ++i) {
             LandmarkObs obs;
             obs.x = x_sense[i];
             obs.y = y_sense[i];
