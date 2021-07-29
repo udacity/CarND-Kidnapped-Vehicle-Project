3437d8f4ccf88e7edcd7ac39446ea3d989b505cd
Peravene Raveendran <peravene@yahoo.de>
2021-07-29 19:21:10 +0200

    fill init

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -23,15 +23,28 @@ using std::vector;
 
 void ParticleFilter::init(double x, double y, double theta, double std[]) {
   /**
-   * TODO: Set the number of particles. Initialize all particles to 
+   * Set the number of particles. Initialize all particles to 
    *   first position (based on estimates of x, y, theta and their uncertainties
    *   from GPS) and all weights to 1. 
    * TODO: Add random Gaussian noise to each particle.
    * NOTE: Consult particle_filter.h for more information about this method 
    *   (and others in this file).
    */
-  num_particles = 0;  // TODO: Set the number of particles
+  num_particles = 1000;  // TODO: Set the number of particles
 
+  for (int i=0; i<num_particles; i++)
+  {
+    Particle newParticle;
+    newParticle.id = num_particles;
+    newParticle.x = x + static_cast<double>(rand())/static_cast<double>(RAND_MAX/2*std[0]) - std[0];
+    newParticle.y = y + static_cast<double>(rand())/static_cast<double>(RAND_MAX/2*std[1]) - std[1];
+    newParticle.theta = static_cast<double>(rand())/static_cast<double>(RAND_MAX/2*std[2]) - std[2];
+    newParticle.weight = 1;
+
+    particles.push_back(newParticle);
+  }
+
+  is_initialized = true;
 }
 
 void ParticleFilter::prediction(double delta_t, double std_pos[], 
